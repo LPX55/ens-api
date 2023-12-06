508a5d2725377e9ede421b5db7a2966e3c416ab5
LPX <info@shapeshift.one>
2023-12-06 17:28:48 +0900

    fix: cors headers

--- vercel-serverless/api/[input].js
+++ vercel-serverless/api/[input].js
@@ -69,8 +69,16 @@ export default async function handler(req, res) {
 
   // Set CORS headers
   res.setHeader('Cache-Control', 's-maxage=60')
+  res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
-
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
   res.status(200).json(records)
 }
